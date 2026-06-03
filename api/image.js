83023de1074516bbf5d6d2b61b2ee69a2efbcad9
vikingrobert52-stack <vikingrobert52@gmail.com>
2026-06-04 00:41:03 +0200

    Fix Authorization header to use API_KEY directly

--- api/image.js
+++ api/image.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     
     const response = await fetch(url, {
       headers: {
-        'Authorization': `Bearer ${API_KEY}`,
+        'Authorization': API_KEY,
       },
     });
 
